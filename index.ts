996d7ac9ef7408c35c4f848595b40887c3ccbf80
deathabel <deathabel@gmail.com>
2022-08-30 15:49:02 +0800

    2 files modified

--- index.ts
+++ index.ts
@@ -1,31 +1,93 @@
-import {
-  BehaviorSubject,
-  concatMap,
-  distinctUntilChanged,
-  filter,
-  from,
-  fromEvent,
-  Observable,
-  of,
-  Subject,
-  switchMap,
-} from 'rxjs';
-import { map, debounceTime, tap } from 'rxjs/operators';
-
-const obs = [];
-// 1.
-// obs[0] =
-
-// 2.
-// document.getElementById('input2')
-// obs[1] = fromEvent(, 'keyup').pipe();
-
-// 3
-const input2Subject = new Subject<number>();
-// obs[2] = input2Subject.pipe();
-
-console.log('=========== Result ============');
-if (obs.length > 0) {
-  const resultObs = obs[obs.length - 1];
-  resultObs.subscribe(console.log);
+/** Observer interface */
+interface Observer {
+  /** 實作nofity function，提供Observable呼叫 */
+  notify(message: any): void;
 }
+/** Observable interface */
+class Observable {
+  private observers: Observer[] = [];
+  /**  通知所有的觀察者，並提供異動的資料 */
+  notifyObservers(message: any): void {
+    for (var observer of this.observers)
+      new Promise((resolve, reject) => {
+        try {
+          resolve(observer.notify(message));
+        } catch (ex) {
+          reject(ex);
+        }
+      });
+  }
+  /** 提供Observer訂閱 */
+  subscribe(observer: Observer): Subscription {
+    return new Subscription(this, this.observers.push(observer) - 1);
+  }
+  /** 移除Observer訂閱 */
+  removeObserver(index: number) {
+    this.observers.splice(index, 1);
+  }
+}
+/** Subscription interface */
+class Subscription {
+  constructor(private observable: Observable, private index: number) {
+    this.observable = observable;
+    this.index = index;
+  }
+  /** 提供Observer退訂 */
+  unsubscribe(): void {
+    this.observable.removeObserver(this.index);
+  }
+}
+
+class Respository {
+  private stock: number = 0;
+  stockChanged$ = new Observable();
+
+  getStock(): number {
+    return this.stock;
+  }
+
+  changeStock(command: {
+    type: 'input' | 'plus' | 'reduce';
+    value: number;
+  }): void {
+    this.stock = command.value;
+    this.stockChanged$.notifyObservers(command);
+  }
+}
+const repository = new Respository();
+// input element
+document.getElementById('inputElement').addEventListener('keyup', (event) => {
+  if (event.key !== 'Enter') return;
+  const value = parseInt((event.target as HTMLInputElement).value) || 0;
+  repository.changeStock({ type: 'input', value: value });
+});
+// plus & reduce button event;
+document.querySelectorAll('input[type="button"]').forEach((node) => {
+  node.addEventListener('click', (event) => {
+    const elementValue = (event.target as HTMLInputElement).value;
+    const type = elementValue === '+' ? 'plus' : 'reduce';
+    const value = repository.getStock() + (elementValue === '+' ? 1 : -1);
+    repository.changeStock({ type, value });
+  });
+});
+// input observer
+repository.stockChanged$.subscribe({
+  notify: (command) => {
+    if (command.type === 'input') return;
+    (document.getElementById('inputElement') as HTMLInputElement).value =
+      command.value;
+  },
+});
+// result observer
+repository.stockChanged$.subscribe({
+  notify: (command) => {
+    document.getElementById('result').innerHTML = command.value;
+  },
+});
+// log observer
+repository.stockChanged$.subscribe({
+  notify: (command) => {
+    document.querySelector(`[obs-type='${command.type}']`).innerHTML +=
+      command.value + '<br>';
+  },
+});
